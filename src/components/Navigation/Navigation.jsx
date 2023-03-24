d281b040c6460c193c5460ef22eb0f8d7e4161d0
Vlad-Kolyada <107812458+daddy-our-bandera@users.noreply.github.com>
2023-03-24 04:42:07 +0200

    Create Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { useAuth } from 'hooks';
+import { Link } from 'components/AuthNavigate/AuthNavigate.styled';
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      {isLoggedIn && <Link to="/contacts">Contacts</Link>}
+    </nav>
+  );
+};
